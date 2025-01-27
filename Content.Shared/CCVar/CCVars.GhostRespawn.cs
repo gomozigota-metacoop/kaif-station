9c7a10dd6203c6993bcdf333087b0d4709aa42c5
ILLUZORR <92508353+ILLUZORR@users.noreply.github.com>
2025-01-27 05:47:32 +0500

    Update CCVars.GhostRespawn.cs

--- Content.Shared/CCVar/CCVars.GhostRespawn.cs
+++ Content.Shared/CCVar/CCVars.GhostRespawn.cs
@@ -5,11 +5,11 @@ namespace Content.Shared.CCVar;
 public sealed partial class CCVars
 {
     public static readonly CVarDef<int> GhostRespawnTime =
-        CVarDef.Create("ghost.respawn_time", 15, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("ghost.respawn_time", 15, CVar.NOTIFY | CVar.REPLICATED);
 
     public static readonly CVarDef<int> GhostRespawnMaxPlayers =
-        CVarDef.Create("ghost.respawn_max_players", 40, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("ghost.respawn_max_players", 40, CVar.NOTIFY | CVar.REPLICATED);
 
     public static readonly CVarDef<bool> GhostAllowSameCharacter =
-        CVarDef.Create("ghost.allow_same_character", false, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("ghost.allow_same_character", false, CVar.NOTIFY | CVar.REPLICATED);
 }
